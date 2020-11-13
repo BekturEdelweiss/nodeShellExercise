82c5df14fe6a278dac8847191d40f26725c15aba
Bektur Elebesov <bektur.elebesov@gmail.com>
2020-11-13 15:41:09 -0600

    bashpwd

---
+++ bash.js
@@ -0,0 +1,10 @@
+process.stdout.write("prompt > ");
+process.stdin.on("data", (data) => {
+    const cmd = data.toString().trim()
+    
+   
+    process.stdout.write("you typed: " + cmd)
+    process.stdout.write("\nprompt > ");
+})
+
+const pwd = require("./pwd")
